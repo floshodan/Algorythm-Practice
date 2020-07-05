44d7692e5f1bfbf08e0db21d07e0f2ce8f63fac2
floshodan <florian.schneller@gmx.de>
2020-07-05 20:28:23 +0200

    [initial commit]

---
+++ A_Watermelon/solution.py
@@ -0,0 +1,15 @@
+
+# Watermelon in kg must be devided evenly among 2 people the 
+# e.g 12 can be devided by 8 and 4 or 6 and 6
+
+input = input()
+input = int(input)
+print(input % 2 == 0)
+
+
+if (input % 2 == 0 and input != 2):
+	print('Yes')
+else:
+	print('No')
+	
+
---
+++ A_Way_Too_Long_Words/solution.py
@@ -0,0 +1,13 @@
+
+word = input()
+max_len = 10
+
+
+if (len(word) >= max_len):
+	new_word = word[0]+str(len(word)-2)+word[-1]
+	print(new_word)
+else:
+	print(word)
+
+
+
